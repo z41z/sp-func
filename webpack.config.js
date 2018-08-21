f950ebc28ad62b594a697386b806df898315d17d
r3qu3stt1me0ut <r3qu3stt1me0ut@gmail.com>
2018-08-21 10:04:06 +0800

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, ""),
+    filename: 'index.min.js',
+    library: "__FUNC",
+  },
+  module: {}
+};
