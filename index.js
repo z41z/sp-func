a0b4a70d7be2eaca9b37f96ae600ad5a8837b3ea
r3qu3stt1me0ut <r3qu3stt1me0ut@gmail.com>
2018-08-24 17:23:35 +0800

    remove loadDate

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports.loadData = require('./fetch/index');
+// module.exports.loadData = require('./fetch/index');
 module.exports.serviceAPI = require('./fetch/serviceAPI');
 module.exports.Validate = require('./validate/index');
 module.exports.Url = require('./url/index');
